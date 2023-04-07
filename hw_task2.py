7c1dc3c972d1eba5138732c48b43cdea73563af0
Davyd.AR <9766288@mail.ru>
2023-04-07 23:57:29 +0300

    finish task 2

--- hw_task2.py
+++ hw_task2.py
@@ -1,3 +1,14 @@
 #  Задача 2: Найдите сумму цифр трехзначного числа.
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+s = 0
+n = int(input())
+if n > 99 and n < 1000:
+    for i in range(3):
+        s = s + n % 10
+        n = int(n/10)
+        print(s, " ", n)
+    print(s)
+else:
+    print("Error input")
