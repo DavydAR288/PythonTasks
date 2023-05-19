3e1d897e1e73877cd45684927eea1048040d2a38
Davyd.AR <9766288@mail.ru>
2023-05-19 23:43:32 +0300

    Add Create PY files

---
+++ seminar7.py
@@ -0,0 +1,35 @@
+#lambda, filter, map, zip, enumerate, list comprehension
+
+#list comprehension
+# a = [i if i < 3 else 0 for i in range(5)]
+# print(a)
+# for i in range(6):
+#     a.append(5)
+# print(a)
+
+#enumerate
+# a = [0, 1, 2, 3, 2, 5, 1]
+# for indx,val in enumerate(a):
+#     print(indx,val)
+
+#zip
+# a = (1,2,4,5,6)
+# b = "abcd"
+# f = {45:"b",54:"c",87:"fr"}
+# for i1,i2,i3 in zip(a,b,f.values()):
+#     print(i1,i2,i3)
+
+#lambda
+# def summa(a,b):
+#     return a+b
+# a = lambda x,y: x+y if x == 3 else 0
+
+#map
+# a = [1,2,3,4,5,6]
+# a = list(map(lambda el: el if el%2==0 else 0,a))
+# print(a)
+
+#filter
+# a = [1, 2, 3, 4, 5, 6]
+# a = list(filter(lambda el: True if el%2==0 else False,a))
+# print(a)
---
+++ task51.py
@@ -0,0 +1,26 @@
+# Напишите функцию same_by(characteristic, objects), которая проверяет, все ли объекты имеют одинаковое значение
+# некоторой характеристики, и возвращают True, если это так. Если значение характеристики для разных объектов
+# отличается - то False. Для пустого набора объектов, функция должна возвращать True. Аргумент characteristic - это функция,
+# которая принимает объект и вычисляет его характеристику.
+
+# Ввод:							Вывод:
+# values = [0, 2, 10, 6]				same
+# if same_by(lambda x: x % 2, values):
+# 	print(‘same’)
+# else:
+# 	print(‘different’)
+
+
+# values = [0, 2, 10, 6]
+# same_by = list(filter(lambda a: True if a % 2 == 0 else False, values))
+# if len(values) == len(same_by):
+#     print('same')
+# else:
+#     print('different')
+
+
+values = [3, 5, 1]
+if same_by(lambda x: True if x % 2 == 1 else False, values):
+    print('same')
+else:
+    print('different')
